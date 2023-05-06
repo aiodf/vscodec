c64b8632f8f65c4c124408f4531536d03afb5852
aiodf <2498235007@qq.com>
2023-05-06 19:35:49 +0800

    0.0.A

--- leetcode/1.c
+++ leetcode/1.c
@@ -2,7 +2,7 @@
  * @Author: aiodf
  * @Date: 2023-05-06 15:16:22
  * @LastEditors: aiodf
- * @LastEditTime: 2023-05-06 19:26:49
+ * @LastEditTime: 2023-05-06 19:35:40
  * @FilePath: \vscodec\leetcode\1.c
  * @Description: 滑动算法
  */
@@ -32,6 +32,7 @@ struct Heap {
 //1
 //2
 //3
+//4
 void init(struct Heap* obj, int capacity) {
     obj->size = 0;
     obj->heap = NULL;
